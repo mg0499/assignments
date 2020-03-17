e6bc63ebb07ac0ea09fb49e82d457c4c6ba8690d
mg0499 <62252000+mg0499@users.noreply.github.com>
2020-03-17 19:56:26 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -41,7 +41,8 @@ def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
     ['China', 'Korea, South', 'Cruise Ship', 'Italy', 'Iran']
     >>> top5_countries_by_date(12, 3)
     ['China', 'Italy', 'Iran', 'Korea, South', 'France']
-
+ 
+ 
     :param day: 4 digit integer representation of the year to get the countries for, defaults to 2020
     :param month: Day of month to get the countries for as an integer indexed from 1
     :param year: Month to get the countries for as an integer indexed from 1
